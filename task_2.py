82d2cd8ffa1bea7f11f335de4d7d0402513c9b86
Chris <yuxiaohui0626@gmail.com>
2022-10-16 17:28:19 +0100

    Task 2

--- task_2.py
+++ task_2.py
@@ -5,16 +5,20 @@ import constant
 import matplotlib.pyplot as plt
 import numpy as np
 
+original_data, original_sample_rate = util.reader(constant.ORIGINAL_VIDEO_URL)
+original_time, original_time_amplitude = util.cal_time_domain(original_data, original_sample_rate)
+original_time_amplitude = original_time_amplitude / np.max(original_time_amplitude)
+original_frequency, original_frequency_amplitude = util.cal_frequency_domain_db(original_data, original_sample_rate)
+original_frequency = np.log10(original_frequency)
+VOWEL_AMPLITUDE_LOW_THRESHOLD = 115
 CONSONANT_AMPLITUDE_LOW_THRESHOLD = 110
-SPEECH_AMPLITUDE_LOW_THRESHOLD = 0
-original_data, sample_rate = util.reader(constant.ORIGINAL_VIDEO_URL)
-frequency, amplitude = util.cal_frequency_domain_db(original_data, sample_rate)
+VOICE_AMPLITUDE_LOW_THRESHOLD = 0.06
 
 
 def cal_vowel_frequency_peak():
     base_path = "data/vowel"
     filenames = os.listdir(base_path)
-    vowel_frequency = []
+    vowel_frequency_peaks = []
 
     for filename in filenames:
         if ".wav" not in filename:
@@ -22,16 +26,27 @@ def cal_vowel_frequency_peak():
         data, sample_rate = util.reader(os.path.join(base_path, filename))
 
         frequency, amplitude = util.cal_frequency_domain(data, sample_rate)
-        # ft = np.fft.fft(data)  # 傅里叶变换
-        # frequency = np.fft.fftfreq(data.size, d=1.0 / sample_rate)  # 频率
-        # frequency = frequency[:int(len(frequency) / 2)]  # 由于对称性，只取一半区间
-        #
-        # amplitude = np.abs(ft)  # 取绝对值
-        # amplitude = amplitude[:int(len(amplitude) / 2)]  # 由于对称性，只取一半区间
-
+        # Search vowel frequency peak through amplitude
         peak_idxs = signal.find_peaks(amplitude[:2500], distance=1000)[0]
-        vowel_frequency += list(frequency[peak_idxs])
-    return vowel_frequency
+        vowel_frequency_peaks += list(frequency[peak_idxs])
+    return vowel_frequency_peaks
+
+
+def cal_vowel_frequency_range():
+    base_path = "data/vowel"
+    filenames = os.listdir(base_path)
+    vowel_frequency_range = []
+
+    for filename in filenames:
+        if ".wav" not in filename:
+            continue
+        data, sample_rate = util.reader(os.path.join(base_path, filename))
+
+        frequency, amplitude = util.cal_frequency_domain_db(data, sample_rate)
+        # Search consonant frequency range through amplitude
+        idxs = amplitude > VOWEL_AMPLITUDE_LOW_THRESHOLD
+        vowel_frequency_range.append(frequency[idxs])
+    return vowel_frequency_range
 
 
 def cal_consonant_frequency_range():
@@ -47,7 +62,6 @@ def cal_consonant_frequency_range():
         frequency, amplitude = util.cal_frequency_domain_db(data, sample_rate)
         # Search consonant frequency range through amplitude
         idxs = amplitude > CONSONANT_AMPLITUDE_LOW_THRESHOLD
-        print(len(frequency[idxs]))
         consonant_frequency_range.append(frequency[idxs])
     return consonant_frequency_range
 
@@ -55,20 +69,34 @@ def cal_consonant_frequency_range():
 # Mark the frequency peaks of vowel in original video
 def mark_vowel_frequency_peak():
     vowel_frequency_peaks = cal_vowel_frequency_peak()
-    idxs = np.searchsorted(frequency, vowel_frequency_peaks)
-    plt.plot(frequency, amplitude)
-    plt.plot(frequency[idxs], amplitude[idxs], 'r.')
+    idxs = np.searchsorted(original_frequency, vowel_frequency_peaks)
+    plt.plot(original_frequency, original_frequency_amplitude)
+    plt.plot(original_frequency[idxs], original_frequency_amplitude[idxs], 'r.')
+    plt.xlabel('Frequency')
+    plt.ylabel('Amplitude')
+    plt.show()
+
+
+def mark_vowel_frequency_range():
+    vowel_frequency_range = cal_vowel_frequency_range()
+    idxs = np.array([], dtype=int)
+    for frequency_range in vowel_frequency_range:
+        idxs = np.append(idxs, np.searchsorted(original_frequency, frequency_range))
+    plt.plot(original_frequency, original_frequency_amplitude)
+    plt.plot(original_frequency[idxs], original_frequency_amplitude[idxs], 'r.')
+    plt.xlabel('Frequency')
+    plt.ylabel('Amplitude')
     plt.show()
 
 
 # Mark the frequency range of consonant in original video
 def mark_consonant_frequency_range():
     consonant_frequency_range = cal_consonant_frequency_range()
-    idxs = np.array([], dtype=int)
-    for frequency_range in consonant_frequency_range:
-        idxs = np.append(idxs, np.searchsorted(frequency, frequency_range))
-    plt.plot(frequency, amplitude)
-    plt.plot(frequency[idxs], amplitude[idxs], 'r.')
+    plt.plot(original_frequency, original_frequency_amplitude)
+    for range_item in consonant_frequency_range:
+        range_item = np.log10(range_item)
+        idxs = np.searchsorted(original_frequency, range_item)
+        plt.plot(original_frequency[idxs], original_frequency_amplitude[idxs], 'r.')
     plt.xlabel('Frequency')
     plt.ylabel('Amplitude')
     plt.show()
@@ -76,18 +104,18 @@ def mark_consonant_frequency_range():
 
 # Mark the frequency range of vowel and consonant in original video
 def mark_vowel_and_consonant_frequency_range():
-    time, amplitude = util.cal_time_domain(original_data, sample_rate)
-    idxs = abs(amplitude) > SPEECH_AMPLITUDE_LOW_THRESHOLD
-    time = time[idxs]
-    amplitude = amplitude[idxs]
-    plt.plot(time, amplitude)
-    plt.xlabel('Time')
+    frequency_range = np.append(cal_consonant_frequency_range(), cal_vowel_frequency_range())
+    # frequency_range = cal_consonant_frequency_range()
+    plt.plot(original_frequency, original_frequency_amplitude)
+    for range_item in frequency_range:
+        range_item = np.log10(range_item)
+        idxs = np.searchsorted(original_frequency, range_item)
+        plt.plot(original_frequency[idxs], original_frequency_amplitude[idxs], 'r.')
+    plt.xlabel('Frequency')
     plt.ylabel('Amplitude')
     plt.show()
 
 
-
-
-# mark_vowel_frequency_peak()
-# mark_consonant_frequency_range()
+mark_vowel_frequency_peak()
+mark_consonant_frequency_range()
 mark_vowel_and_consonant_frequency_range()
